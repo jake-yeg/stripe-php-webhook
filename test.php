16b9f97f0f5eae9f62161e4b8160f1c2ffd0ad17
Jake <jnealyeg@gmail.com>
2017-04-29 14:19:07 -0600

    Delete test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config = require_once('webhook_config.php');
-
-if($config['log_type'] === 1){
-    $config['db'] = new PDO("mysql:host={$config['db_host']};dbname={$config['db_name']}", $config['db_user'], $config['db_pass']);
-}
-
-var_dump($config);
